ecd60ada2bd769e9fe26f84a400dac2f45479197
youngkwon02 <youngkwon02@naver.com>
2021-05-17 13:47:39 +0900

    create: Main app

--- LoveLikeLion/settings.py
+++ LoveLikeLion/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +108,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
+
 
 USE_I18N = True
 
