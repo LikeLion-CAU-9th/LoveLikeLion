3036bf51b11481fce6e99f72bfecaa27a8d1a77d
youngkwon02 <youngkwon02@naver.com>
2021-05-20 12:29:08 +0900

    refactor: Timer.js

--- main/static/js/timer.js
+++ main/static/js/timer.js
@@ -14,18 +14,18 @@ console.log(targetDate);
 let targetTime = targetDate.getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function () {
+let x = setInterval(function () {
   // Get todays date and time
-  var now = new Date().getTime();
+  let now = new Date().getTime();
 
   // Find the distance between now an the count down date
-  var distance = targetTime - now;
+  let distance = targetTime - now;
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   //   // Display the result in an element with id="demo"
   //   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
